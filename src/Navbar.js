d800343f0fec17707816fbf6f898ee0449e25275
Gokul-24-2001 <gokulashok1002@gmail.com>
2025-01-16 19:55:22 +0530

    modified navbar file

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,7 +5,8 @@ export default function Navbar(){
         <div className="p-1 d-flex gap-3 border border-2 bg- $light ">
             <h5><MdMovieCreation ></MdMovieCreation></h5> 
             <Link className="text-decoration-none" to="/"><h5>Home</h5></Link>
-            <Link   className="text-decoration-none" to="/watchlist"><h5>watchlist</h5></Link>
+            <Link className="text-decoration-none" to="/watchlist"><h5>watchlist</h5></Link>
+            <Link className="text-decoration-none" to="/login"><h5>login</h5></Link>
         </div>
     )
 }
